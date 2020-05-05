95c6db1fe00dac8faddc773d6738d24c5bb7ee97
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-05 14:28:37 -0400

    Added call to fix bug

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -42,6 +42,7 @@ class AudioRecorderController: UIViewController {
                                                                    weight: .regular)
         loadAudio()
         updateViews()
+        try? prepareAudioSession() 
     }
     
     deinit {
